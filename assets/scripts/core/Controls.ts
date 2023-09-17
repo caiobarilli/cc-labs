28939715a540e25a8383567a1f59c3ad0654f0f2
Caio Barilli <caiobarilli@gmail.com>
2023-09-17 04:06:28 -0300

    [doc] add doc on code

--- assets/scripts/core/Controls.ts
+++ assets/scripts/core/Controls.ts
@@ -18,11 +18,20 @@ export class Controls extends Component {
   })
   public player: Player;
 
+  /**
+   * @en
+   * This function is called when the scene is loading.
+   * create a input listener for the keyboard.
+   */
   onLoad() {
     input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
     input.on(Input.EventType.KEY_UP, this.onKeyUp, this);
   }
 
+  /**
+   * @en
+   * This function is called when keyboard is pressed.
+   */
   onKeyDown(event: EventKeyboard) {
     switch (event.keyCode) {
       case KeyCode.SPACE:
@@ -40,6 +49,10 @@ export class Controls extends Component {
     }
   }
 
+  /**
+   * @en
+   * This function is called when keyboard is released.
+   */
   onKeyUp(event: EventKeyboard) {
     switch (event.keyCode) {
       case KeyCode.SPACE:
