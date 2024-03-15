db2f04a7f878e275d251485d0b15670665b5ee3f
Caio Barilli <caiobarilli@gmail.com>
2024-03-14 23:49:42 -0300

    feat: stop animation with button

--- spin-wheel-test/assets/game.ts
+++ spin-wheel-test/assets/game.ts
@@ -1,14 +1,46 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Button, Sprite, Animation } from "cc";
 const { ccclass, property } = _decorator;
 
-@ccclass('game')
+@ccclass("game")
 export class game extends Component {
-    start() {
+  @property({
+    group: { name: "UI Buttons" },
+    tooltip: "Set the stop button",
+    type: Button,
+  })
+  stop: Button = null;
 
-    }
+  @property({
+    tooltip: "Set the center wheel node",
+    type: Node,
+  })
+  public centerWhell: Node;
 
-    update(deltaTime: number) {
-        
-    }
+  public sprite: Sprite;
+  public animation: Animation;
+
+  public isStoped: boolean | undefined;
+
+  onLoad(): void {
+    this.sprite = this.getComponent(Sprite);
+    this.animation = this.getComponent(Animation);
+  }
+
+  start() {
+    let stopButton = this.stop.node;
+    stopButton.on(Button.EventType.CLICK, () => {
+      this.onStop();
+    });
+  }
+
+  /**
+   * @en
+   */
+  onStop() {
+    this.isStoped = true;
+    const animation = this.centerWhell.getComponent(Animation);
+    animation && animation.stop();
+  }
+
+  update(deltaTime: number) {}
 }
-
