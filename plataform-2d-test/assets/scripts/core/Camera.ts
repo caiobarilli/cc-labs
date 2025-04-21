b133bc564c974daa4277d702a1ed590b958f8787
Caio Barilli <caiobarilli@gmail.com>
2025-04-21 00:21:45 -0300

    fix update camera

--- plataform-2d-test/assets/scripts/core/Camera.ts
+++ plataform-2d-test/assets/scripts/core/Camera.ts
@@ -1,75 +1,56 @@
-import { _decorator, Component, Vec3, view } from "cc";
-import { Utils } from "./Utils";
+import { _decorator, Component, Vec3, warn } from "cc";
 import { Player } from "../Player";
 const { ccclass, property } = _decorator;
 
 @ccclass("Camera")
 export class Camera extends Component {
-  @property({
-    group: { name: "Camera nodes" },
-    tooltip: "Add player node",
-    type: Player,
-  })
-  public target: Player;
+  @property({ type: Player })
+  public target: Player | null = null;
 
-  public utils: Utils;
-  public worldPosition: Vec3;
+  @property
+  public verticalOffset: number = 280;
 
-  public screenSize: number[] = [];
+  @property
+  public horizontalDeadZone: number = 50;
 
-  /**
-   * @en
-   * Creates a new instance of Utils.
-   * Set the screen size.
-   */
-  onLoad() {
-    this.utils = new Utils();
-    this.screenSize = [
-      view.getVisibleSize().width,
-      view.getVisibleSize().height,
-    ];
-  }
+  @property
+  public followSpeed: number = 5;
+
+  private targetPos: Vec3 = new Vec3();
+
+  private currentPos: Vec3 = new Vec3();
 
   /**
    * @en
    * This function is called every frame.
    */
-  update() {
-    this.worldPosition = this.target.node.getWorldPosition();
+  update(dt: number) {
 
-    if (this.targetInHalfScreen(this.worldPosition)) {
-      this.focusTargetOnCenter();
-    } else {
-      this.target.onGround && this.focusTargetOnRight();
+    console.log(this.targetPos.y)
+
+    if (!this.target) {
+      warn("Camera: Target não atribuído.");
+
+      return;
     }
-  }
 
-  /**
-   * @en
-   */
-  targetInHalfScreen(targetPosX: Vec3) {
-    if (targetPosX.x > this.utils.getScreenPercentage(this.screenSize[0], 50)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+    const playerPos = this.target.node.getPosition();
+    const cameraPos = this.node.getPosition();
 
-  /**
-   * @en
-   */
-  focusTargetOnCenter() {
-    this.node.position = new Vec3(
-      this.target.node.position.x,
-      this.worldPosition.y - 90,
-      0
-    );
-  }
+    const dx = playerPos.x - cameraPos.x;
 
-  /**
-   * @en
-   */
-  focusTargetOnRight() {
-    this.node.position = new Vec3(0, this.worldPosition.y - 90, 0);
+    this.targetPos.x = Math.abs(dx) > this.horizontalDeadZone
+      ? playerPos.x - Math.sign(dx) * this.horizontalDeadZone
+      : cameraPos.x;
+
+    this.targetPos.y = playerPos.y + this.verticalOffset;
+
+    this.targetPos.z = 0;
+
+    const smoothFactor = Math.min(dt * this.followSpeed, 1);
+
+    Vec3.lerp(this.currentPos, cameraPos, this.targetPos, smoothFactor);
+
+    this.node.setPosition(this.currentPos);
   }
 }
